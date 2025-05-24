1dfe57df33d2956e500ed116e9f7e1137e2f489a
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2025-05-24 19:26:40 +0530

    project(aws): edit Using LocalStack for AWS Lambda with SNS trigger

--- aws/localstack-lambda-with-sns-trigger/src/main/java/com/example/SnsRequestHandler.java
+++ aws/localstack-lambda-with-sns-trigger/src/main/java/com/example/SnsRequestHandler.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SNSEvent;
 
@@ -11,7 +10,6 @@ public class SnsRequestHandler implements RequestHandler<SNSEvent, List<String>>
 
 	@Override
 	public List<String> handleRequest(SNSEvent event, Context context) {
-		final LambdaLogger logger = context.getLogger();
 		final List<String> messages = event.getRecords().stream()
 				.map(SNSEvent.SNSRecord::getSNS)
 				.map(SNSEvent.SNS::getMessage)
